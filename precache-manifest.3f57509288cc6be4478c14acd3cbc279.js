f2404c04908bd7ef3d2ecd7c3d3a820f908a5c67
Gustavo Sanchez <sanchezmartingustavo@gmail.com>
2021-02-03 17:57:06 -0300

    Updates

---
+++ precache-manifest.3f57509288cc6be4478c14acd3cbc279.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a66e9997c77f803618c7b92e957b6ab6",
+    "url": "/infinite-scroll-movies/index.html"
+  },
+  {
+    "revision": "eb22677c1338a609489e",
+    "url": "/infinite-scroll-movies/static/css/main.720611a5.chunk.css"
+  },
+  {
+    "revision": "c6a09113febaaf2405a1",
+    "url": "/infinite-scroll-movies/static/js/2.8d3cbbf0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/infinite-scroll-movies/static/js/2.8d3cbbf0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb22677c1338a609489e",
+    "url": "/infinite-scroll-movies/static/js/main.1a7b622e.chunk.js"
+  },
+  {
+    "revision": "0f7ed798f9a44fccce9e",
+    "url": "/infinite-scroll-movies/static/js/runtime-main.65da0f22.js"
+  },
+  {
+    "revision": "5eb0b8830d1a853ce96e4c6e44994a44",
+    "url": "/infinite-scroll-movies/static/media/posterplaceholder.5eb0b883.jpg"
+  }
+]);
